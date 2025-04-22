1929190195c98c3b88a43eea5acdc4f09a416234
Dean Aviv <deanaviv5@gmail.com>
2025-04-22 16:03:14 +0300

    feat: add AdbDeviceInfo model class
    Created a data model to store both device ID and model name for connected Android devices.

---
+++ gui/ApkInstaller.GUI/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ApkInstaller.GUI
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly AdbDeviceManager _adbManager = new();
+
+        public ObservableCollection<AdbDeviceInfo> Devices { get; } = new();
+
+        public ICommand RefreshDevicesCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            RefreshDevicesCommand = new RelayCommand(RefreshDevices);
+        }
+
+        private void RefreshDevices()
+        {
+            Console.WriteLine("RefreshDevices() called"); 
+
+            Devices.Clear();
+            var found = _adbManager.GetConnectedDevices();
+
+            foreach (var device in found)
+            {
+                Console.WriteLine($"Found device: {device}");
+                Devices.Add(device);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
