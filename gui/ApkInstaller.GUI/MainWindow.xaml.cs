32fa2a0cdd6171a62a3696ee71125f6a38dc7893
Dean Aviv <deanaviv5@gmail.com>
2025-04-22 16:05:51 +0300

    feat: set DataContext in MainWindow.xaml.cs to use MVVM

--- gui/ApkInstaller.GUI/MainWindow.xaml.cs
+++ gui/ApkInstaller.GUI/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ApkInstaller.GUI
 {
@@ -19,13 +10,14 @@ namespace ApkInstaller.GUI
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
 
         private void SelectFilesButton_Click(object sender, RoutedEventArgs e)
         {
             var installWindow = new InstallWindow();
             installWindow.Show();
-            this.Hide(); // Optional
+            this.Hide(); 
         }
     }
 }
