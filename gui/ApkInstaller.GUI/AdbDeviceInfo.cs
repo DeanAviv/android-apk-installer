c5cff82fff4ee163dadb086efbed16c8f64c99a8
Dean Aviv <deanaviv5@gmail.com>
2025-04-22 16:04:44 +0300

    feat: add AdbDeviceInfo model class
    Created a simple model to hold device ID and model for display in the

---
+++ gui/ApkInstaller.GUI/AdbDeviceInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace ApkInstaller.GUI
+{
+    public class AdbDeviceInfo
+    {
+        public string DeviceId { get; set; } = "";
+        public string Model { get; set; } = "";
+
+        public override string ToString() => $"{Model} ({DeviceId})";
+    }
+}
